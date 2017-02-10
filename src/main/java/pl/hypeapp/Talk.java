8fea91315ec505a918e4aa0a436fd410a6a015ab
Przemek <pszem.szym@gmail.com>
2017-02-10 18:30:28 +0100

    Created talk model

---
+++ src/main/java/pl/hypeapp/Talk.java
@@ -0,0 +1,28 @@
+package pl.hypeapp;
+
+/**
+ * Created by Przemysław Szymkowiak on 29.01.2017.
+ */
+
+public class Talk {
+
+    private String id;
+
+    private String topic;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
