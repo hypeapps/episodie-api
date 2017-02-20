4ef0f02c19c25b96cd27a6520e087dc150dd5bd8
Przemek <pszem.szym@gmail.com>
2017-02-20 18:47:15 +0100

    Created clean architecture project

---
+++ application/configuration/src/main/java/pl/hypeapp/episodie/EpisodieApiApplication.java
@@ -0,0 +1,19 @@
+package pl.hypeapp.episodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EpisodieApiApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpisodieApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EpisodieApiApplication.class);
+    }
+}
