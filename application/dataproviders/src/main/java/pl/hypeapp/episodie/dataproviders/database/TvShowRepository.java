e16487a347af84b0c263e6a1e471ded0acfe251e
PrzemekEnterprise <pszem.szym@gmail.com>
2017-04-05 19:29:33 +0200

    Fix

--- application/dataproviders/src/main/java/pl/hypeapp/episodie/dataproviders/database/TvShowRepository.java
+++ application/dataproviders/src/main/java/pl/hypeapp/episodie/dataproviders/database/TvShowRepository.java
@@ -1,6 +1,7 @@
 package pl.hypeapp.episodie.dataproviders.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.hypeapp.core.entity.tvmaze.TvShow;
 
-public interface TvShowRepository extends JpaRepository<TvShowRepository, String> {
+public interface TvShowRepository extends JpaRepository<TvShow, String> {
 }
