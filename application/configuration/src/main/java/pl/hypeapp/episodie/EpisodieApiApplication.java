b4a3bde3d03d61d15940bb7b21c47c3284db88be
PrzemekEnterprise <pszem.szym@gmail.com>
2017-04-05 20:26:33 +0200

    Added annotations to application class

--- application/configuration/src/main/java/pl/hypeapp/episodie/EpisodieApiApplication.java
+++ application/configuration/src/main/java/pl/hypeapp/episodie/EpisodieApiApplication.java
@@ -1,11 +1,19 @@
 package pl.hypeapp.episodie;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "pl.hypeapp.episodie.configuration")
+@EnableAdminServer
+@EnableJpaRepositories
 public class EpisodieApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
