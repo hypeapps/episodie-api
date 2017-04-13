c53b47ce9f03a741b4eb5762cff7ea2002dfd1c9
PrzemekEnterprise <pszem.szym@gmail.com>
2017-04-13 20:22:08 +0200

    Specified query for looking that Is tv show in database.

--- application/dataproviders/src/main/java/pl/hypeapp/episodie/dataproviders/database/TvShowRepository.java
+++ application/dataproviders/src/main/java/pl/hypeapp/episodie/dataproviders/database/TvShowRepository.java
@@ -1,7 +1,14 @@
 package pl.hypeapp.episodie.dataproviders.database;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.hypeapp.core.entity.tvmaze.TvShow;
 
-public interface TvShowRepository extends JpaRepository<TvShow, Integer> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.hypeapp.core.entity.database.TvShowLocal;
+
+public interface TvShowRepository extends JpaRepository<TvShowLocal, Integer> {
+
+    @Query("SELECT CASE  WHEN count(tv)> 0 THEN true ELSE false END FROM TvShowLocal tv WHERE tv.tvShowApiId = :id")
+    boolean existsByTvShowApiId(@Param("id") String tvShowApiId);
+
 }
