621d011e7ddf1f631dc21cc779ac05bd8619c426
PrzemekEnterprise <pszem.szym@gmail.com>
2017-04-21 19:11:55 +0200

    Removed Logger.

--- application/configuration/src/main/java/pl/hypeapp/episodie/configuration/job/JobSchedulerConfiguration.java
+++ application/configuration/src/main/java/pl/hypeapp/episodie/configuration/job/JobSchedulerConfiguration.java
@@ -11,8 +11,6 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class JobSchedulerConfiguration {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(JobSchedulerConfiguration.class);
-
     @Bean
     public ScheduledExecutorService scheduledExecutorService(ScheduledJob... jobs) {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
@@ -21,7 +19,6 @@ public class JobSchedulerConfiguration {
             long initialDelay = job.getInitialDelay();
             long period = job.getPeriod();
             TimeUnit timeUnit = job.getTimeUnit();
-//            LOGGER.info("Scheduling job {} to run every {} {}, after an initial {} {}", job.getName(), period, timeUnit, initialDelay, timeUnit);
             scheduledExecutorService.scheduleAtFixedRate(job, initialDelay, period, timeUnit);
         }
 
