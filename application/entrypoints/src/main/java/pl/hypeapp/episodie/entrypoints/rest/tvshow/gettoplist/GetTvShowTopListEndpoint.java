9a4865792d3d3448553cfe0dc20f6dee3e26ae11
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-04 21:00:14 +0200

    Created tv show top list endpoint.

---
+++ application/entrypoints/src/main/java/pl/hypeapp/episodie/entrypoints/rest/tvshow/gettoplist/GetTvShowTopListEndpoint.java
@@ -0,0 +1,36 @@
+package pl.hypeapp.episodie.entrypoints.rest.tvshow.gettoplist;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.hypeapp.core.entity.database.TvShowLocal;
+import pl.hypeapp.core.usecase.tvshow.toplist.gettoplist.GetTvShowTopListUseCase;
+import pl.hypeapp.core.usecase.tvshow.toplist.gettoplist.ResourceNotFoundException;
+import pl.hypeapp.episodie.entrypoints.rest.exception.NotFoundException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class GetTvShowTopListEndpoint {
+
+    private static final String API_PATH = "api/tvshow/toplist";
+
+    private final GetTvShowTopListUseCase getTvShowTopListUseCase;
+
+    public GetTvShowTopListEndpoint(GetTvShowTopListUseCase getTvShowTopListUseCase) {
+        this.getTvShowTopListUseCase = getTvShowTopListUseCase;
+    }
+
+    @RequestMapping(value = API_PATH, method = GET)
+    public Page<TvShowLocal> getTopList(Pageable pageableRequest) {
+        Page<TvShowLocal> tvShowTopList;
+        try {
+            tvShowTopList = getTvShowTopListUseCase.getTopList(pageableRequest);
+        } catch (ResourceNotFoundException e) {
+            throw new NotFoundException();
+        }
+        return tvShowTopList;
+    }
+
+}
