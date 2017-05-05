4070e74a231d05bfa4fa0c5adb48c1b45da86247
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-05 14:25:35 +0200

    Added test for top list database provider.

--- application/dataproviders/src/test/java/pl/hypeapp/episodie/dataproviders/database/toplist/TvShowTopListDatabaseProviderTest.java
+++ application/dataproviders/src/test/java/pl/hypeapp/episodie/dataproviders/database/toplist/TvShowTopListDatabaseProviderTest.java
@@ -1,20 +1,24 @@
 package pl.hypeapp.episodie.dataproviders.database.toplist;
 
 import org.junit.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import pl.hypeapp.core.entity.database.TvShowLocal;
 import pl.hypeapp.core.entity.database.TvShowTopListLocal;
-import pl.hypeapp.episodie.dataproviders.database.tvshow.TvShowRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
 public class TvShowTopListDatabaseProviderTest {
-    private TvShowRepository tvShowRepository = mock(TvShowRepository.class);
+
     private TvShowTopListRepository tvShowTopListRepository = mock(TvShowTopListRepository.class);
 
-    private TvShowTopListDatabaseProvider tvShowTopListDatabaseProvider = new TvShowTopListDatabaseProvider(tvShowRepository,
-            tvShowTopListRepository);
+    private TvShowTopListDatabaseProvider tvShowTopListDatabaseProvider = new TvShowTopListDatabaseProvider(tvShowTopListRepository);
 
     @Test
-    public void shouldInsertTvShowToTopList() {
+    public void shouldInsertTvShowToTopList() throws Exception {
         TvShowTopListLocal tvShowTopListLocal = new TvShowTopListLocal();
 
         when(tvShowTopListRepository.save(tvShowTopListLocal)).thenReturn(tvShowTopListLocal);
@@ -22,6 +26,32 @@ public class TvShowTopListDatabaseProviderTest {
         tvShowTopListDatabaseProvider.insert(tvShowTopListLocal);
 
         verify(tvShowTopListRepository, times(1)).save(tvShowTopListLocal);
+    }
+
+    @Test
+    public void shouldReturnsTopList() throws Exception {
+        Pageable pageable = new PageRequest(0, 3);
+
+        tvShowTopListDatabaseProvider.getTopList(pageable);
+
+        verify(tvShowTopListRepository, times(1)).findAll(pageable);
+    }
+
+    @Test
+    public void shouldReturnsTvShowsTopList() throws Exception {
+        List<String> topListIds = new ArrayList<>();
+        topListIds.add("12");
+        topListIds.add("13");
+
+        List<TvShowLocal> tvShows = new ArrayList<>();
+        tvShows.add(new TvShowLocal());
+        tvShows.add(new TvShowLocal());
+
+        when(tvShowTopListRepository.getTopListTvShows(topListIds)).thenReturn(tvShows);
+
+        tvShowTopListDatabaseProvider.getTopListTvShows(topListIds);
+
+        verify(tvShowTopListRepository, times(1)).getTopListTvShows(topListIds);
     }
 
 }
