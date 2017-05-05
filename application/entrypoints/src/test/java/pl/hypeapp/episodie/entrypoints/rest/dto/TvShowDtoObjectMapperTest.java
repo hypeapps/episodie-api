d92f370ba8d53c27f092823788ec73a8cd8bcb93
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-05 18:02:41 +0200

    Crated test for to DTO object mapper.

---
+++ application/entrypoints/src/test/java/pl/hypeapp/episodie/entrypoints/rest/dto/TvShowDtoObjectMapperTest.java
@@ -0,0 +1,37 @@
+package pl.hypeapp.episodie.entrypoints.rest.dto;
+
+import org.junit.Test;
+import pl.hypeapp.core.entity.database.EpisodeLocal;
+import pl.hypeapp.core.entity.database.SeasonLocal;
+import pl.hypeapp.core.entity.database.TvShowLocal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertSame;
+
+public class TvShowDtoObjectMapperTest {
+
+    private TvShowDtoObjectMapper tvShowDtoObjectMapper = new TvShowDtoObjectMapper();
+
+    @Test
+    public void shouldMapLocalToDto() throws Exception {
+        String tvMazeId = "82";
+        List<EpisodeLocal> episodeLocals = new ArrayList<>();
+        episodeLocals.add(new EpisodeLocal());
+
+        List<SeasonLocal> seasonLocals = new ArrayList<>();
+        seasonLocals.add(new SeasonLocal());
+
+        TvShowLocal tvShowLocal = TvShowLocal.builder()
+                .tvShowApiId(tvMazeId)
+                .episodes(episodeLocals)
+                .seasons(seasonLocals)
+                .build();
+
+        TvShowDto tvShowDto = tvShowDtoObjectMapper.tvShowLocalToDto.apply(tvShowLocal);
+
+        assertSame(tvShowDto.getTvShowApiId(), tvShowLocal.getTvShowApiId());
+    }
+
+}
