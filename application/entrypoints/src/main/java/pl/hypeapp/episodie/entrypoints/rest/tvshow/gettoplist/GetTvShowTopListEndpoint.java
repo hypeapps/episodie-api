8f18e03270e0950780864c8b34154e3d96410a4d
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-10 16:29:38 +0200

    Changed: endpoints returns tv show dto.

--- application/entrypoints/src/main/java/pl/hypeapp/episodie/entrypoints/rest/tvshow/gettoplist/GetTvShowTopListEndpoint.java
+++ application/entrypoints/src/main/java/pl/hypeapp/episodie/entrypoints/rest/tvshow/gettoplist/GetTvShowTopListEndpoint.java
@@ -1,13 +1,19 @@
 package pl.hypeapp.episodie.entrypoints.rest.tvshow.gettoplist;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.hypeapp.core.entity.database.TvShowLocal;
 import pl.hypeapp.core.usecase.tvshow.ResourceNotFoundException;
 import pl.hypeapp.core.usecase.tvshow.toplist.gettoplist.GetTvShowTopListUseCase;
+import pl.hypeapp.episodie.entrypoints.rest.dto.TvShowDto;
+import pl.hypeapp.episodie.entrypoints.rest.dto.TvShowDtoObjectMapper;
 import pl.hypeapp.episodie.entrypoints.rest.exception.NotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -23,14 +29,22 @@ public class GetTvShowTopListEndpoint {
     }
 
     @RequestMapping(value = API_PATH, method = GET)
-    public Page<TvShowLocal> getTopList(Pageable pageableRequest) {
+    public Page<TvShowDto> getTopList(Pageable pageableRequest) {
         Page<TvShowLocal> tvShowTopList;
         try {
             tvShowTopList = getTvShowTopListUseCase.getTopList(pageableRequest);
+            return toDto(tvShowTopList, pageableRequest);
         } catch (ResourceNotFoundException e) {
             throw new NotFoundException();
         }
-        return tvShowTopList;
+    }
+
+    private Page<TvShowDto> toDto(Page<TvShowLocal> tvShowTopList, Pageable pageableRequest) {
+        TvShowDtoObjectMapper objectMapper = new TvShowDtoObjectMapper();
+        List<TvShowDto> tvShowDtos = tvShowTopList.getContent().stream()
+                .map(tvShow -> objectMapper.tvShowLocalToDto.apply(tvShow))
+                .collect(Collectors.toList());
+        return new PageImpl<>(tvShowDtos, pageableRequest, tvShowTopList.getTotalElements());
     }
 
 }
