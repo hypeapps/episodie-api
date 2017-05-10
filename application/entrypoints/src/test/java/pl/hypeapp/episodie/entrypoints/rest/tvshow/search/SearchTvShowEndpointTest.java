c282b7f8d02a4e39274f7b158f8d5afaa27f4c5b
PrzemekEnterprise <pszem.szym@gmail.com>
2017-05-10 15:28:16 +0200

    Created test for search endpoint.

---
+++ application/entrypoints/src/test/java/pl/hypeapp/episodie/entrypoints/rest/tvshow/search/SearchTvShowEndpointTest.java
@@ -0,0 +1,64 @@
+package pl.hypeapp.episodie.entrypoints.rest.tvshow.search;
+
+import org.junit.Test;
+import pl.hypeapp.core.entity.database.EpisodeLocal;
+import pl.hypeapp.core.entity.database.SeasonLocal;
+import pl.hypeapp.core.entity.database.TvShowLocal;
+import pl.hypeapp.core.usecase.tvshow.ResourceNotFoundException;
+import pl.hypeapp.core.usecase.tvshow.search.SearchTvShowUseCase;
+import pl.hypeapp.episodie.entrypoints.rest.dto.TvShowDto;
+import pl.hypeapp.episodie.entrypoints.rest.exception.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.*;
+
+public class SearchTvShowEndpointTest {
+
+    private SearchTvShowUseCase searchTvShowUseCase = mock(SearchTvShowUseCase.class);
+
+    private SearchTvShowEndpoint searchTvShowEndpoint = new SearchTvShowEndpoint(searchTvShowUseCase);
+
+    @Test
+    public void shouldReturnSearchResult() throws Exception {
+        String query = "tvshowquery";
+
+        List<EpisodeLocal> episodeLocals = new ArrayList<>();
+        episodeLocals.add(new EpisodeLocal());
+        List<SeasonLocal> seasonLocals = new ArrayList<>();
+        seasonLocals.add(new SeasonLocal());
+        List<TvShowLocal> result = new ArrayList<>();
+
+        TvShowLocal tvShowLocal = TvShowLocal.builder()
+                .tvShowApiId("82")
+                .seasons(seasonLocals)
+                .episodes(episodeLocals)
+                .build();
+
+        result.add(tvShowLocal);
+
+        when(searchTvShowUseCase.search(query)).thenReturn(result);
+
+        List<TvShowDto> tvShowDtos = searchTvShowEndpoint.search(query);
+
+        assertSame(tvShowDtos.get(0).getTvShowApiId(), result.get(0).getTvShowApiId());
+        verify(searchTvShowUseCase, times(1)).search(query);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void shouldThrowException() throws Exception {
+        String query = "";
+
+        ResourceNotFoundException resourceNotFoundException = new ResourceNotFoundException();
+
+        when(searchTvShowUseCase.search(query)).thenThrow(resourceNotFoundException);
+
+        searchTvShowUseCase.search(query);
+
+        assertThatExceptionOfType(NotFoundException.class).isThrownBy(() -> searchTvShowEndpoint.search(query));
+    }
+
+}
