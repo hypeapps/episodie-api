73c793fac8f005b7ce9c946214fa7a721d9998b8
PrzemekEnterprise <pszem.szym@gmail.com>
2017-06-24 16:36:24 +0200

    Change: spring app to servlet type.

--- application/configuration/src/main/java/pl/hypeapp/episodie/EpisodieApiApplication.java
+++ application/configuration/src/main/java/pl/hypeapp/episodie/EpisodieApiApplication.java
@@ -3,6 +3,8 @@ package pl.hypeapp.episodie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,17 +14,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "pl.hypeapp.episodie.configuration")
 @EnableJpaRepositories(basePackages = "pl.hypeapp.episodie.dataproviders")
 @EntityScan("pl.hypeapp.episodie.core.entity")
-public class EpisodieApiApplication
-//        extends SpringBootServletInitializer
-{
+public class EpisodieApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EpisodieApiApplication.class, args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(EpisodieApiApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EpisodieApiApplication.class);
+    }
 
 }
